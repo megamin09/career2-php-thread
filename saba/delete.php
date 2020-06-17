4684c71212c03d2a1a2063ff6e4e7e6f398e9899
2019 DSP6101 <2019DSP6101@ntsv6101.com>
2020-06-17 09:48:10 +0900

    Change file deletion data

--- saba/delete.php
+++ saba/delete.php
@@ -3,8 +3,8 @@
 $txt_box_file = '../txt_box.txt';
 
 $fp = fopen($txt_box_file, 'w');
-//なにもデータがない場合0を代入しておく
-fwrite($fp, "　");
+//なにもデータがない場合なにもいれない
+fwrite($fp, "");
 fclose($fp);
 
 header( "Location: ./../index.php" ) ;
