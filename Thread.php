60cebb19c581407060f4e8d369ca269e72957fea
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-07-01 11:38:21 +0900

    Teread add

--- Thread.php
+++ Thread.php
@@ -13,14 +13,76 @@ class Thread
 
     public function getList()
     {
-        return file_get_contents(self::THREAD_FILE);
+        //ファイルの有無を確認
+        if( file_exists("txt_box.txt") )
+        {
+            $fp = fopen("txt_box.txt", 'r');
+
+            while($line = fgetcsv($fp))
+            {
+                // 読み込んだ結果を表示します。
+                if( $line[0]=== "　" )
+                {
+                    //データがなかったため表示させない
+                    break;
+                }
+
+                $id = 0;
+                $data = "<hr>";
+                $data = $data.'<p>投稿日時: '.$line[$id].'<br></p>';
+                $id++;
+                $data = $data.'<p>投稿者: '.$line[$id].'</p>';
+                $data = $data.'<p>内容</p>';
+                $id++;
+                $line[$id] = str_replace( "< br >", "\n", $line[$id] );
+                // $line[$id] = str_replace( "\n", "< br >", $line[$id] );
+                // $line[$id] = str_replace( "\r", "< br>", $line[$id] );
+                $line[$id] = nl2br($line[$id]);
+                $data = $data.'<p>'.$line[$id]."<br></p>";
+
+                echo $data;
+            }
+
+            fclose($fp);
+        }
     }
 
-    public function post() {
+    public function post( string $name_box, string $txt_box ) 
+    {
+        $txt_box_file = 'txt_box.txt';
+        // $name_box = $_POST["name"];
+        // $txt_box = $_POST["box"];
 
+        //echo '投稿者:'.$name_box.'<br>';
+        //echo 'コメント<br>'.$txt_box;
+
+        //ファイルが存在しない場合作成する
+        if(! file_exists($txt_box_file) )
+        {
+            $fp = fopen($txt_box_file, 'w');
+            fclose($fp);
+        }
+        //読み込みと書き込みができる状態でファイルを開く
+        $fp = fopen( $txt_box_file, 'ab' );
+        fwrite( $fp, date( "Y/m/d H:i:s" ).", " );
+        fwrite( $fp, $name_box.", " );
+        //$txt_box = nl2br($txt_box);
+        $txt_box = str_replace( "\r\n", "< br >", $txt_box );
+        $txt_box = str_replace( "\n", "< br >", $txt_box );
+        $txt_box = str_replace( "\r", "< br >", $txt_box );
+        fwrite( $fp, $txt_box."\n" );
+        fclose($fp);
     }
 
-    public function delete() {
+    public function delete() 
+    {
+        $txt_box_file = 'txt_box.txt';
 
+        $fp = fopen($txt_box_file, 'w');
+        //なにもデータがない場合なにもいれない
+        fwrite($fp, "");
+        fclose($fp);
+
+        exit ;
     }
 }
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  */
 
 session_start();
-new Thread();
+// new Thread();
 
 function setToken()
 {
@@ -73,9 +73,11 @@ body,td,th {font-family:"ほのかアンティーク角","ＭＳ Ｐゴシック
 
 <h2>スレッド</h2>
 
-<form method="POST" action="saba/delete.php">
-    <button type="submit">投稿を全削除する</button>
+<form method="POST" action = "<?php print($_SERVER['PHP_SELF']) ?>">
+<input type="hidden" name="method" value="DELETE">
+            <button type="submit">投稿を全削除する</button>
 </form>
+
 
 <?php
 
@@ -85,85 +87,27 @@ const THREAD_FILE = 'thread.txt';
 require_once './Thread.php';
 $thread = new Thread('掲示板App');
 
-function WRITE_data()
+//送信があったら実行
+if( $_SERVER["REQUEST_METHOD"] === "POST" )
 {
-    $txt_box_file = 'txt_box.txt';
-    $name_box = $_POST["name"];
-    $txt_box = $_POST["box"];
-
-    //echo '投稿者:'.$name_box.'<br>';
-    //echo 'コメント<br>'.$txt_box;
-
-    //ファイルが存在しない場合作成する
-    if(! file_exists($txt_box_file) )
+    if (isset($_POST["method"]) && $_POST["method"] === "DELETE") 
     {
-        $fp = fopen($txt_box_file, 'w');
-        fclose($fp);
+        $thread->delete();
     }
-    //読み込みと書き込みができる状態でファイルを開く
-    $fp = fopen( $txt_box_file, 'ab' );
-    fwrite( $fp, date( "Y/m/d H:i:s" ).", " );
-    fwrite( $fp, $name_box.", " );
-    //$txt_box = nl2br($txt_box);
-    $txt_box = str_replace( "\r\n", "< br >", $txt_box );
-    $txt_box = str_replace( "\n", "< br >", $txt_box );
-    $txt_box = str_replace( "\r", "< br >", $txt_box );
-    fwrite( $fp, $txt_box."\n" );
-    // fputcsv( $fp, $txt_box );
-
-    $_POST = array();
-    
-    // //二重投稿を防ぐため
-    // header('Location: ./');
+    else if(  array_key_exists('name', $_POST) )
+    {
+        $thread->post( $_POST["name"], $_POST["box"] );
+    }
 
     // ブラウザのリロード対策
     $redirect_url = $_SERVER['HTTP_REFERER'];
     header("Location: $redirect_url");
     exit;
-
-    fclose($fp);
 }
 
-//送信があったら実行
-if( $_SERVER["REQUEST_METHOD"] === "POST" && array_key_exists('name', $_POST) )
-{
-    WRITE_data();
-}
 
-//ファイルの有無を確認
-if( file_exists("txt_box.txt") )
-{
-    $fp = fopen("txt_box.txt", 'r');
 
-    while($line = fgetcsv($fp))
-    {
-        // 読み込んだ結果を表示します。
-        if( $line[0]=== "　" )
-        {
-            //データがなかったため表示させない
-            break;
-        }
-
-        $id = 0;
-        $data = "<hr>";
-        $data = $data.'<p>投稿日時: '.$line[$id].'<br></p>';
-        $id++;
-        $data = $data.'<p>投稿者: '.$line[$id].'</p>';
-        $data = $data.'<p>内容</p>';
-        $id++;
-        $line[$id] = str_replace( "< br >", "\n", $line[$id] );
-        // $line[$id] = str_replace( "\n", "< br >", $line[$id] );
-        // $line[$id] = str_replace( "\r", "< br>", $line[$id] );
-        $line[$id] = nl2br($line[$id]);
-        $data = $data.'<p>'.$line[$id]."<br></p>";
-
-        echo $data;
-    }
-
-    fclose($fp);
-}
-
-echo $thread->getList();
+$thread->getList();
 
 ?>
 
